0f799f83c0d85640f94e0308dbc67bcc53c7eda9
Sinan <snnhbyb@gmail.com>
2022-06-29 13:15:22 +0200

    still issue

---
+++ src/main/java/com/example/minitodomanagement/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.example.minitodomanagement.repository;
+
+
+import com.example.minitodomanagement.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TodoRepository extends JpaRepository<Todo, Long > {
+    List<Todo> findByUserName(String user);
+}
